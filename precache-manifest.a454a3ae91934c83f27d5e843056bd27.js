58da05f1fbf398c40cfb047e88ff943a6f3587b5
andytheelf <ballew22@gmail.com>
2020-09-01 16:52:25 -0500

    Updates

---
+++ precache-manifest.a454a3ae91934c83f27d5e843056bd27.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdbff6ae6b1e47371fc41aede426afa1",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "d3c7962fce89d4453677",
+    "url": "/react-portfolio/static/css/2.06c478a6.chunk.css"
+  },
+  {
+    "revision": "32188dc8ae4e45b48dd5",
+    "url": "/react-portfolio/static/css/main.531dbbcb.chunk.css"
+  },
+  {
+    "revision": "d3c7962fce89d4453677",
+    "url": "/react-portfolio/static/js/2.08883c5c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio/static/js/2.08883c5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32188dc8ae4e45b48dd5",
+    "url": "/react-portfolio/static/js/main.63c269e5.chunk.js"
+  },
+  {
+    "revision": "42e6e07fd3dcb766db6b",
+    "url": "/react-portfolio/static/js/runtime-main.14f01a53.js"
+  },
+  {
+    "revision": "61ba6f971a87f47b7b5273ce57a835ab",
+    "url": "/react-portfolio/static/media/cloud2.61ba6f97.jpg"
+  }
+]);
